5eb46d736c4ecf698153923c800da4c13a27a3ce
pandelevanhochust <toan.lv225160@sis.hust.edu.vn>
2026-06-24 14:45:41 +0700

    updated: change usrp ip

--- scripts/run_yolo_web.sh
+++ scripts/run_yolo_web.sh
@@ -5,8 +5,8 @@ export LD_LIBRARY_PATH=$QNN:$LD_LIBRARY_PATH
 export ADSP_LIBRARY_PATH=$QNN/hexagon-v68/unsigned
 export XDG_RUNTIME_DIR=/dev/socket/weston
 export WAYLAND_DISPLAY=wayland-1
-cd /opt/MyVersion/build
-MODEL=${1:-../models/yolov8_w8a8.bin}
+cd /opt/YOLO8_VNPT/build
+MODEL=${1:-../yolov8_int8.bin}
 PORT=${2:-8080}
 exec ./cam_yolo_web_tracking "$MODEL" "$PORT"
 
